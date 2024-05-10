a082d73ecc026e95f6da689ebf63442df3c5337d
superbabii <nzrbabii@gmail.com>
2024-05-10 19:56:19 +0300

    init

---
+++ PowerBoard.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PowerBoard.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PowerBoard.X.production.hex
